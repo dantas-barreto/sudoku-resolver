fea478ae1093709fa3bacab8be34afdb9893f81c
astrolia <astrolia22@gmail.com>
2024-03-06 15:35:45 -0300

    Update unocand.c

--- unocand.c
+++ unocand.c
@@ -7,15 +7,15 @@ void unocand(int A[TAM][TAM], int qual[9][9][TAM],int t_sleep, int n_sleep, int 
 bool Exif[9][9], bool Exic[9][9], bool Exis[9][9], int Nprch, int p, int strng, int xch, int ych)
 {
 int i, j;
-    for(i = 0; i < TAM; i++) // linha
+    for(i = 0; i < TAM; i++) 
     {
-        for(j = 0; j < TAM; j++) // coluna
-        {
+        for(j = 0; j < TAM; j++)
+        {                         // percorre as linhas e colunas da matriz A
             if( A[i][j] == 0) // se o espaço estiver vazio
             {
                 if( qual[i][j][9] == 1) // verifica se exite apenas um candidato
                 {
-                    Post(qual[i][j][0],i,j,Fa,qual,Fa_f,Fa_c,Fa_s,Exif,Exic,Exis,A,Nprch); // atualiza o posição
+                    Post(qual[i][j][0],i,j,Fa,qual,Fa_f,Fa_c,Fa_s,Exif,Exic,Exis,A,Nprch); // atualiza a posição
                 }
             }
         }
